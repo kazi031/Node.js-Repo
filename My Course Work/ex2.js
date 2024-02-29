b67f366f552c2130b4c84aef5ee9f38ff048d099
kazi031 <kazirafidraiyan031@gmail.com>
2024-02-29 15:33:24 +0600

    Stream

--- My Course Work/ex2.js
+++ My Course Work/ex2.js
@@ -4,50 +4,64 @@
 var util = require("util");
 var path = require("path");
 var fs = require("fs");
+var Transform = require("stream").Transform;
 
 // var getStdin = require("get-stdin");
 
 var args = require("minimist")( process.argv.slice(2), {
-	boolean: ["help", "in"],
+	boolean: ["help", "in", "out"],
         string: ["file"]
 } );
+
+
 
 var BASE_PATH = path.resolve(
 	process.env.BASE_PATH || __dirname
 );
 
+var OUTFILE = path.join(BASE_PATH, "out.txt");
+
 // console.log(args);
 
-import("get-stdin").then((getStdin) => {
+
 	if (args.help) {
 	  printHelp();
 	} else if (args.in || args._.includes("-")) {
-		if (typeof getStdin === "function") {
-			getStdin().then(processFile).catch(error);
-		  } else {
-			error("getStdin is not a function.");
-		  }
+		processFile(process.stdin);
 	} else if (args.file) {
-	  fs.readFile(path.join(BASE_PATH, args.file), function onContents(err, contents) {
-		if (err) {
-		  error(err.toString());
-		} else {
-		  processFile(contents.toString());
-		}
-	  });
+		let stream = fs.createReadStream(path.join(BASE_PATH, args.file))
+		processFile(stream)
 	} else {
-	  error("Incorrect Usage.", true);
+	  	error("Incorrect Usage.", true);
 	}
-  }).catch((err) => {
-	console.error("Error loading get-stdin:", err);
-  });
+  
 // printHelp();
 
 
 
-function processFile(contents) {
-	contents = contents.toUpperCase();
-	process.stdout.write(contents);
+function processFile(inStream) {
+	var outStream = inStream;
+
+	var upperStream = new Transform({
+		transform(chunk,enc,cb){
+			this.push(chunk.toString().toUpperCase());
+			setTimeout(cb,500);
+			cb();
+		}
+	});
+
+	outStream = outStream.pipe(upperStream);
+
+
+	var targetStream;
+	if (args.out) {
+		targetStream = process.stdout
+	} else {
+		targetStream = fs.createWriteStream(OUTFILE);
+	}
+	
+	process.stdout;
+	outStream.pipe(targetStream);
 	// console.log(contents); // prints hex data
 	 
 }
@@ -68,5 +82,6 @@ function printHelp() {
     console.log("--help               print this help");
     console.log("--file={FILENAME}    process the file");
 	console.log("--in, -              process stdin");
+	console.log("--out 				  print to stdout");
     console.log("");
 }
---
+++ My Course Work/test.js
@@ -0,0 +1,11 @@
+var stream1; // readable
+var stream2; // writable
+var stream4;
+var stream5;
+var stream6;
+
+var stream3 = 
+stream1
+.pipe(stream4)
+.pipe(stream5)
+.pipe(stream6)
